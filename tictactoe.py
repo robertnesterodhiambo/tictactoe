7f4af340f8464062ee7927db2cd658602bcabc63
Robert Nester Odhiambo <robertnesterodhiambo@gmail.com>
2024-01-15 22:10:21 +0300

    board display

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,13 @@
 # board
+board =  ["_","_","_",
+          "_","_","_",
+          "_","_","_"]
+def display_board():
+    print(" | " + board[0] + " | ", board[1] + " | ", board[2] + " | ")
+    print(" | " + board[3] + " | ", board[4] + " | ", board[5] + " | ")
+    print(" | " + board[6] + " | ", board[7] + " | ", board[8] + " | ")
+
+display_board()
 #display board
 # play game
 # handle turns 
